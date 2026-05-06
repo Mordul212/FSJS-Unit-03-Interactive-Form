79c0786fa64e40b0fd8ffb43e38283586025b390
Mordul212 <103167502+Mordul212@users.noreply.github.com>
2026-05-06 14:11:47 -0500

    updated to exceeding expectations standards
    updated readme.md to reflect additional changes
    
    Form works correctly with no unexpected behaviour

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FSJS-Unit-03-Interactive-Form
 
+added real-time error messages for required fields
+this is in addition to validation check during form submission
+
+added custom error message if email field has partial match
--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,4 @@
-console.log('test');
+// console.log('test');
 
 const nameField = document.querySelector('input[type="text"]');
 const otherJobRole = document.getElementById('other-job-role');
@@ -11,6 +11,21 @@ const paymentSelectionBox = document.getElementById('payment');
 const creditCardDiv = document.getElementById('credit-card');
 const paypalDiv = document.getElementById('paypal');
 const bitcoinDiv = document.getElementById('bitcoin');
+const form = document.querySelector('form');
+const activityList = registerFieldset.querySelectorAll('input[type="checkbox"]'); // currently all input fields in the activites field set are checkboxes, but instructions mention checkboxes specifically, so I will be strict in selection
+
+function errorMessageDisplayCheck(elementToAdjustClass, validData, hintLocation, e){ // need to call event since function has been moved to global scope
+    if (validData) {
+        document.getElementById(`${hintLocation}-hint`).style.display = 'none'; // hide error hint
+        elementToAdjustClass.classList.add('valid');
+        elementToAdjustClass.classList.remove('not-valid');
+    } else {
+        e.preventDefault(); // prevent form submission
+        document.getElementById(`${hintLocation}-hint`).style.display = 'block'; // display error hint
+        elementToAdjustClass.classList.add('not-valid');
+        elementToAdjustClass.classList.remove('valid');
+    }  
+}
 
 //initialize page
 nameField.focus(); // focus on first required text field
@@ -62,9 +77,9 @@ registerFieldset.addEventListener('change', (e) => {
     }
 });
 
-paymentSelectionBox.firstElementChild.nextElementSibling.setAttribute('selected', '');
-paypalDiv.style.display = 'none';
-bitcoinDiv.style.display = 'none';
+paymentSelectionBox.firstElementChild.nextElementSibling.setAttribute('selected', ''); // initialize credit card as default payment method
+paypalDiv.style.display = 'none'; //hide paypal fields when not selected
+bitcoinDiv.style.display = 'none'; //hide bitcoin fields when not selected
 paymentSelectionBox.addEventListener('change', (e) => {
     if (paymentSelectionBox.value === 'credit-card') {
         creditCardDiv.style.display = 'block';
@@ -80,3 +95,94 @@ paymentSelectionBox.addEventListener('change', (e) => {
         bitcoinDiv.style.display = 'block';
     }
 }); 
+
+form.addEventListener('submit', (e) => {
+    const validateText = /[^\W*]/; // simple word regex - exclude whitespace and special characters
+    const validateEmail = /[^\W*]@[^\W*]\.[^\W*]/; // simple email regex
+    let activityCheckedCount = 0;
+
+    errorMessageDisplayCheck(nameField.closest('label'), validateText.test(nameField.value),'name', e); // check required name field
+    errorMessageDisplayCheck(document.getElementById('email').closest('label'), validateEmail.test(document.getElementById('email').value),'email', e); // check required email field
+    
+    for (i=0; i < activityList.length; i++) { // how many possible checkboxes
+        if (activityList[i].checked) {
+            activityCheckedCount++;
+        }
+    }
+    errorMessageDisplayCheck(registerFieldset, activityCheckedCount,'activities', e); // check required activity field has at least one activity checked
+    if (paymentSelectionBox.value === 'credit-card') {
+        errorMessageDisplayCheck(document.getElementById('cc-num').closest('label'), /\d{13,16}/.test(document.getElementById('cc-num').value), 'cc', e); // check if credit card number is between 13 and 16 digits
+        errorMessageDisplayCheck(document.getElementById('zip').closest('label'), /\d{5}/.test(document.getElementById('zip').value), 'zip', e); // check if zip code is 5 digits
+        errorMessageDisplayCheck(document.getElementById('cvv').closest('label'), /\d{3}/.test(document.getElementById('cvv').value), 'cvv', e); // check if cvv is 3 digits
+    }
+});
+
+nameField.addEventListener('keyup', (e) => {
+    const validateText = /[^\W*]/; // simple word regex - exclude whitespace and special characters
+    errorMessageDisplayCheck(nameField.closest('label'), validateText.test(nameField.value),'name', e); // check required name field
+});
+
+document.getElementById('email').addEventListener('keyup', (e) => {
+    const validateEmail = /[^\W*]@[^\W*]\.[^\W*]/; // simple email regex
+    const partialEmail = /[^@]/;
+    if (validateEmail.test(document.getElementById('email').value)) {
+        errorMessageDisplayCheck(document.getElementById('email').closest('label'), validateEmail.test(document.getElementById('email').value),'email', e); // check required email field
+        document.getElementById('email-hint').textContent = 'Email address must be formatted correctly';
+    } else if (partialEmail.test(document.getElementById('email').value)) {
+        errorMessageDisplayCheck(document.getElementById('email').closest('label'), false,'email', e); // fail email check
+        document.getElementById('email-hint').textContent = 'Please use "test@example.com" format';
+    }
+    
+});
+
+registerFieldset.addEventListener('change', (e) => {
+    let activityCheckedCount =0;
+    for (i=0; i < activityList.length; i++) { // how many possible checkboxes
+        if (activityList[i].checked) {
+            activityCheckedCount++;
+        }
+    }
+    errorMessageDisplayCheck(registerFieldset, activityCheckedCount,'activities', e); // check required activity field has at least one activity checked
+});
+
+document.getElementById('cc-num').addEventListener('keyup', (e) => {
+    errorMessageDisplayCheck(document.getElementById('cc-num').closest('label'), /\d{13,16}/.test(document.getElementById('cc-num').value), 'cc', e);
+});
+
+document.getElementById('zip').addEventListener('keyup', (e) => {
+    errorMessageDisplayCheck(document.getElementById('zip').closest('label'), /\d{5}/.test(document.getElementById('zip').value), 'zip', e);
+});
+
+document.getElementById('cvv').addEventListener('keyup', (e) => {
+    errorMessageDisplayCheck(document.getElementById('cvv').closest('label'), /\d{3}/.test(document.getElementById('cvv').value), 'cvv', e);
+});
+
+activityList.forEach(individualActivity => {
+    individualActivity.addEventListener('focus', () => {
+        individualActivity.closest('label').classList.add('focus');
+    })
+    individualActivity.addEventListener('blur', () => {
+        individualActivity.closest('label').classList.remove('focus');
+    })
+}); // focus on label matches the rest of the webpages focus on element - nice
+
+//Main Conference activity does not have a time? is this supposed to be a fee to simply enter the conference which would make it required?
+// document.getElementById('activities-box').firstElementChild.firstElementChild.checked = true; // initialize this checkbox as required
+// document.getElementById('activities-box').firstElementChild.firstElementChild.setAttribute('disabled', ''); // disable checkbox to not allow deselection
+
+// nice, exact same data-day-and-time for overlapping for direct comparison
+activityList.forEach(individualActivity => {
+    individualActivity.addEventListener('change', (e) => { // add listeners
+        activityList.forEach(conflictingCheck => { // compare target of listener to each activity
+            if (e.target !== conflictingCheck && e.target.dataset.dayAndTime === conflictingCheck.dataset.dayAndTime) {
+                if (e.target.checked) {
+                    conflictingCheck.setAttribute('disabled', '');
+                    conflictingCheck.closest('label').setAttribute('disabled', '');
+                } else {
+                    conflictingCheck.removeAttribute('disabled');
+                    conflictingCheck.closest('label').removeAttribute('disabled');
+                }
+            }        
+        })
+    })
+});
